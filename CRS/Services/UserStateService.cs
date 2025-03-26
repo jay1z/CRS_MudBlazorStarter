4e5412aa44b8c23356a586b4a794db12ff18bb8f
Jason A. Zurowski <jay1_z@yahoo.com>
2025-03-26 16:30:22 -0500

    Enhance code structure and user interface components
    Updated theme settings, improved code readability, and added new dialog components for better user interaction. Introduced methods for data loading and organized logic for clarity. Adjusted control structures for consistency and commented out logging in UserStateService to reflect potential changes in logging strategy. Overall, these changes aim to improve maintainability and user experience.

--- CRS/Services/UserStateService.cs
+++ CRS/Services/UserStateService.cs
@@ -46,9 +46,9 @@ namespace CRS.Services {
         public async Task InitializeAsync() {
             if (_isInitialized) return;
 
-            using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
-            ILogger logger = factory.CreateLogger("UserStateService");
-            logger.LogInformation("Hello World! Logging is {Description}.", "fun");
+            //using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+            //ILogger logger = factory.CreateLogger("UserStateService");
+            //logger.LogInformation("Hello World! Logging is {Description}.", "fun");
 
             var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
             if (authState.User.Identity?.IsAuthenticated == true) {
